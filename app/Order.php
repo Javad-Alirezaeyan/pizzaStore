8f6a9f4ee49fdbf2cb569a9365c32bad8729f564
Javad <alirezaeyan.javad@gmail.com>
2020-06-13 18:44:24 +0430

    Completing 'Save function' for an order in the back-end

--- app/Order.php
+++ app/Order.php
@@ -9,4 +9,11 @@ class Order extends Model
     //
     protected $table = 'orders';
     protected $primaryKey = 'o_id';
+
+   // protected $fillable = ['o_address', 'created_at', 'updated_at'];
+
+    public function get($where = [])
+    {
+
+    }
 }
