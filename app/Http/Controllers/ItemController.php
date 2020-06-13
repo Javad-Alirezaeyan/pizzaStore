db22e553f4bee2f0d7d0db9bde325793d4d8c366
Javad <alirezaeyan.javad@gmail.com>
2020-06-13 10:38:01 +0430

    relate to the previous commit

---
+++ app/Http/Controllers/ItemController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    //
+
+    public function new()
+    {
+        
+    }
+
+    public function save()
+    {
+        
+    }
+
+    public function list()
+    {
+        
+    }
+
+    public function changeState()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+    
+}
